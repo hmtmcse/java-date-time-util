a0fd36257ed55026792f5690f3d8fcea23cff4b1
Touhid Mia <hmtm.cse@gmail.com>
2019-02-24 23:48:50 +0600

    bismillah

--- src/main/java/com/hmtmcse/datetimeutil/DateTimeUtil.java
+++ src/main/java/com/hmtmcse/datetimeutil/DateTimeUtil.java
@@ -1,22 +1,51 @@
 package com.hmtmcse.datetimeutil;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class DateTimeUtil {
 
 
-    public static String formatDateTime(String pattern, LocalDateTime localDateTime){
-        DateTimeFormatter dateTimeFormatter =  DateTimeFormatter.ofPattern(pattern);
-        if (localDateTime == null){
+    public static String formatDateTime(String pattern, LocalDateTime localDateTime) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(pattern);
+        if (localDateTime == null) {
             return null;
         }
         return localDateTime.format(dateTimeFormatter);
     }
 
-    public static String formatCurrentDateTime(String pattern){
+    public static String formatCurrentDateTime(String pattern) {
         return formatDateTime(pattern, LocalDateTime.now());
     }
 
+    public static Date dateToUtcDate(Date date) {
+        if (date == null){
+            return null;
+        }
+        Instant instant = Instant.ofEpochMilli(date.getTime());
+        return localDateTimeToDate(LocalDateTime.ofInstant(instant, ZoneOffset.UTC), ZoneOffset.UTC);
+    }
+
+
+    public static LocalDateTime dateToLocalDateTime(Date date, ZoneId zoneId) {
+        return date == null ? null : (Instant.ofEpochMilli(date.getTime()).atZone(zoneId).toLocalDateTime());
+    }
+
+
+    public static Date localDateTimeToDate(LocalDateTime localDateTime, ZoneId zoneId) {
+        if (localDateTime == null){
+            return null;
+        }
+        return Date.from(localDateTime.atZone(zoneId).toInstant());
+    }
+
+
+    public static LocalDateTime dateToLocalDateTime(Date date) {
+        return dateToLocalDateTime(date, ZoneId.systemDefault());
+    }
 
 }
--- src/main/java/com/hmtmcse/datetimeutil/test/TestMain.java
+++ src/main/java/com/hmtmcse/datetimeutil/test/TestMain.java
@@ -1,8 +1,13 @@
 package com.hmtmcse.datetimeutil.test;
 
+
+import com.hmtmcse.datetimeutil.DateTimeUtil;
+
+import java.util.Date;
+
 public class TestMain {
 
     public static void main(String[] args) {
-
+        System.out.println(DateTimeUtil.dateToUtcDate(new Date()));
     }
 }
