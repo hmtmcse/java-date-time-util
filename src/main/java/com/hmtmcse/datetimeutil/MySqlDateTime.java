668b8bb98ba0676ef6c546e41d9b792026cf88a4
Touhid Mia <touhid@bitmascot.com>
2019-02-25 09:57:34 +0600

    Date

--- src/main/java/com/hmtmcse/datetimeutil/MySqlDateTime.java
+++ src/main/java/com/hmtmcse/datetimeutil/MySqlDateTime.java
@@ -10,10 +10,8 @@ public class MySqlDateTime {
         String start = DateTimeUtil.oldDateFormatter(new Date(), DTUConstent.MY_SQL_DATE_FORMAT) + " 00:00:00";
         String end = DateTimeUtil.oldDateFormatter(new Date(), DTUConstent.MY_SQL_DATE_FORMAT) + " 23:59:59";
         DateSpanData dateSpanData = new DateSpanData();
-
         dateSpanData.setStartDateTime(DateTimeUtil.oldDateParser(start, DTUConstent.MY_SQL_DATE_TIME_FORMAT));
         dateSpanData.setEndDateTime(DateTimeUtil.oldDateParser(end, DTUConstent.MY_SQL_DATE_TIME_FORMAT));
-
         return dateSpanData;
     }
 }
